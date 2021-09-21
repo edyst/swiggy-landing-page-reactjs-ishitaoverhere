6093ad7dfe8ab0681aa489bfb2abca067c19e7cb
ishitaoverhere <68231563+ishitaoverhere@users.noreply.github.com>
2021-09-21 14:24:28 +0530

    final-commit-by-upload

---
+++ final-project-submission/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Features from './Components/Feature/Features';
+import Pocket from './Components/Pocket_Section/Pocket';
+import Footer from './Components/Footer/Footer';
+import Body from './Components/Body/Body';
+function App() {
+  return (
+    <>
+      <Body/>
+      <Features/>
+      <Pocket />
+      <Footer/>
+     
+      
+
+    </>
+  );
+}
+
+export default App;
